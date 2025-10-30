887cfa02bc9cd9d493643fc3d73e0b435ad35aba
Sulthonhanan <hananakhnaf31@gmail.com>
2025-10-30 09:04:36 +0700

    Pembuatan FormDataDiri

---
+++ app/src/main/java/com/example/myapplication/TeksEdit.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.ModifierLocalReadScope
+
+@composable
+fun FormDataDiri(modifier: Modifier){
+    //variable-variable untuk mengingat nilai masukan dari keyboard
+    var textName by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
+
+
+        )
+    }
+
+}
